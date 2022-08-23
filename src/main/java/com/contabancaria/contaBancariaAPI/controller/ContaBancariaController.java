82b20296384ec407daef0164a665573635696880
vitoriajaco <vitoriajaco@gmail.com>
2022-08-23 14:27:58 -0300

    Criando classe controller e service

---
+++ src/main/java/com/contabancaria/contaBancariaAPI/controller/ContaBancariaController.java
@@ -0,0 +1,10 @@
+package com.contabancaria.contaBancariaAPI.controller;
+
+import com.contabancaria.contaBancariaAPI.service.ContaBancariaService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContaBancariaController {
+
+    private ContaBancariaService contaBancariaService;
+}
--- src/main/java/com/contabancaria/contaBancariaAPI/service/ContaBancariaService.java
+++ src/main/java/com/contabancaria/contaBancariaAPI/service/ContaBancariaService.java
@@ -1,7 +1,12 @@
 package com.contabancaria.contaBancariaAPI.service;
 
+import com.contabancaria.contaBancariaAPI.repository.ContaBancariaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContaBancariaService {
+
+    @Autowired
+    private ContaBancariaRepository contaBancariaRepository;
 }
